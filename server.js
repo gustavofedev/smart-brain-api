250f02cc5fe147e98ec21b0ce538461edab121d2
Gustavo Fe <ggomes.fe@gmail.com>
2019-02-12 17:43:57 -0200

    Added morgan to login and volume to auto update changes code

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan')
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+
 
 const db = knex({
   client: 'pg',
@@ -21,6 +23,7 @@ const db = knex({
 
 const app = express();
 
+app.use(morgan('combined'))
 app.use(cors())
 app.use(bodyParser.json());
 
