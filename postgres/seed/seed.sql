a077c96104c3c5d7f52eb326359ca4b2d5dee437
Gustavo Fe <ggomes.fe@gmail.com>
2019-02-13 19:13:24 -0200

    Docker to postgres with seed and tables generated

---
+++ postgres/seed/seed.sql
@@ -0,0 +1,2 @@
+INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+INSERT into login (hash, email) values ('$2a$04$PcbifWu9DVMme0WJwO4j3.t8PuYTmQoMpfESdVjWDDp89PSJHVNte', 'jessie@gmail.com');
